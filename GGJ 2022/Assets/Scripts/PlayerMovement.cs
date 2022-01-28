4d5aed78103d93544a4c48c46631716e612bb691
chrisleimacau <33567290+chrisleimacau@users.noreply.github.com>
2022-01-29 00:36:42 +0800

    Player can move
    Player can now move controlling by WSAD/controller

--- GGJ 2022/Assets/Scripts/PlayerMovement.cs
+++ GGJ 2022/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float movementSpeed = 5f;
+    public Rigidbody2D rb;
+    public Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
     }
 }
