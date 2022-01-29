a50d994c3c795b833141a466372c496a258e0238
SmolIndieGame <smolindiegame@gmail.com>
2022-01-29 12:03:26 +0800

    Create Enemy Spawner

---
+++ GGJ 2022/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : PoolHandler<Enemy>
+{
+    [SerializeField] Enemy enemyPf;
+    [SerializeField] Camera cam;
+    [SerializeField] float interval;
+
+    Watch watch;
+
+    protected override GameObject prefab => enemyPf.gameObject;
+
+    private void Start()
+    {
+        watch = new Watch(interval, true);
+    }
+
+    private void Update()
+    {
+        if (watch.TimeOut)
+        {
+            Vector2 pos = new Vector2();
+
+            int state = Random.Range(0, 4);
+            if (state < 2)
+            {
+                pos.x = Random.value;
+                pos.y = state;
+            }
+            else
+            {
+                pos.y = Random.value;
+                pos.x = state - 2;
+            }
+
+            pos = cam.ViewportToWorldPoint(pos);
+
+            Enemy enemy = Spawn();
+            enemy.transform.position = pos;
+
+            watch.Reset();
+        }
+    }
+}
