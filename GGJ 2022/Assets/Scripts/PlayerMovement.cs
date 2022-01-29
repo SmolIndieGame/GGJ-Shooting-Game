c86f994946a73ed7348c8502755f0a8546d52e67
chrisleimacau <33567290+chrisleimacau@users.noreply.github.com>
2022-01-29 10:28:45 +0800

    Player rotation

--- GGJ 2022/Assets/Scripts/PlayerMovement.cs
+++ GGJ 2022/Assets/Scripts/PlayerMovement.cs
@@ -7,16 +7,29 @@ public class PlayerMovement : MonoBehaviour
     public float movementSpeed = 5f;
     public Rigidbody2D rb;
     public Vector2 movement;
+    public Camera cam;
+    
+    public Vector2 mousePos;
+    public Vector2 lookDir;
+
+    public float angle;
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToViewportPoint(Input.mousePosition);
+        transform.up = mousePos;
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+        
+        //lookDir = mousePos - rb.position;
+        //angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        //rb.rotation = angle;
     }
 }
