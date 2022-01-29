4e85caf11f25289b93f079bbe343a9ed8c9d73a9
Cheoklao <a26482356@gmail.com>
2022-01-29 14:35:41 +0800

    HeartUI

--- GGJ 2022/Assets/Scripts/child.cs
+++ GGJ 2022/Assets/Scripts/child.cs
@@ -6,13 +6,16 @@ public class child : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField]float movespeed=3;
+    [SerializeField] GameObject Hpbar;
     [SerializeField]int Hp=5;
+
+    Watch invincibleTimer;
+    Watch flashingTimer;
     void Start()
     {
-        
+        invincibleTimer = new Watch(3, true);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetKey(KeyCode.A)){
@@ -28,12 +31,19 @@ public class child : MonoBehaviour
             transform.Translate(0,-movespeed*Time.deltaTime,0);
         }   
 
+if (invincibleTimer.TimeOut)
+{
+    GetComponent<BoxCollider2D>().enabled=true;
+}
     }
      void OnCollisionEnter2D(Collision2D other){
-            if (other.gameObject.tag=="Enemy"){
+            if (other.gameObject.tag=="Enemy"){ 
                 ModifyHP(-1);
+                GetComponent<BoxCollider2D>().enabled=false;
+                invincibleTimer.Reset();
             }
         }
+        
     void ModifyHP(int num){
         Hp+=num;
         if (Hp>5){
@@ -44,4 +54,8 @@ public class child : MonoBehaviour
         }
         Debug.Log(Hp);
     }
+
+
+    
+
 }
