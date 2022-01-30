e10fe8de2e10215483afa135157298f109f92635
chrisleimacau <33567290+chrisleimacau@users.noreply.github.com>
2022-01-30 11:16:36 +0800

    Change to racing mode

--- GGJ 2022/Assets/Scripts/Player/WheelChairMovement.cs
+++ GGJ 2022/Assets/Scripts/Player/WheelChairMovement.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class WheelChairMovement : MonoBehaviour
 {
+    [Header("Testing force")]
+    public Vector2 accelerationFactor;
+    public float accelerationInput;
+    public Vector2 force;
+
+    [Header("WheelChairMovement")]
+    public float rotateSpeed;
     public float movementSpeed = 5f;
     public Vector2 childOffset;
     public GameObject child;
@@ -13,23 +20,39 @@ public class WheelChairMovement : MonoBehaviour
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal") * -1;
+        //movement.y = Input.GetAxisRaw("Vertical") * -1;
 
         if (movement != Vector2.zero)
         {
-            transform.up = movement;
+            float angle = movement.x * rotateSpeed;
+            transform.Rotate(0,0,angle);
             child.transform.up = movement;
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            Debug.Log("test");
+            PushForward();
         }
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movementSpeed * Time.fixedDeltaTime * movement.normalized);
+        //rb.MovePosition(rb.position + movementSpeed * Time.fixedDeltaTime * movement.normalized);
+        //PushForward();
     }
 
     void LateUpdate()
     {
         child.transform.position = (Vector2)transform.TransformPoint(childOffset);
+    }
+
+    void PushForward()
+    {
+        Debug.Log("Apply force");
+        force = (Vector2)transform.up * accelerationInput;
+        Debug.Log(force);
+        rb.AddForce(force);
     }
 }
